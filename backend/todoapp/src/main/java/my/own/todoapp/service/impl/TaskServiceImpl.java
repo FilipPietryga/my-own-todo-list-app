e2a0f159bed51c64eeb74f28716bc4dfa63e902a
FilipPietryga <filippatrykpietryga@gmail.com>
2024-01-07 18:33:51 +0100

    introduced TaskRepisotory implementation class

---
+++ backend/todoapp/src/main/java/my/own/todoapp/service/impl/TaskServiceImpl.java
@@ -0,0 +1,38 @@
+package my.own.todoapp.service.impl;
+
+import my.own.todoapp.model.Task;
+import my.own.todoapp.service.TaskService;
+
+import java.util.Collection;
+
+public class TaskServiceImpl implements TaskService {
+    @Override
+    public Task post(Task task) {
+        return null;
+    }
+
+    @Override
+    public Task update(Task task) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public Task get(Long id) {
+        return null;
+    }
+
+    @Override
+    public Collection<Task> getAll() {
+        return null;
+    }
+
+    @Override
+    public Collection<Task> getN(int limit) {
+        return null;
+    }
+}
