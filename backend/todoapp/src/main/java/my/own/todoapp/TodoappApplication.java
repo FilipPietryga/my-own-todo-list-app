06b7e965b7d58b53a78d9855647b797938b45d6b
FilipPietryga <filippatrykpietryga@gmail.com>
2024-01-07 19:27:31 +0100

    added the tasks to the repository on the start of the app

--- backend/todoapp/src/main/java/my/own/todoapp/TodoappApplication.java
+++ backend/todoapp/src/main/java/my/own/todoapp/TodoappApplication.java
@@ -1,5 +1,9 @@
 package my.own.todoapp;
 
+import my.own.todoapp.model.Task;
+import my.own.todoapp.model.TaskResponse;
+import my.own.todoapp.repo.TaskRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,4 +14,12 @@ public class TodoappApplication {
 		SpringApplication.run(TodoappApplication.class, args);
 	}
 
+	CommandLineRunner run(TaskRepository taskRepository) {
+		return args -> {
+			taskRepository.save(new Task(null, "clean the room", "use brush", "2000.11.11 17:15"));
+			taskRepository.save(new Task(null, "clean the dorm", "use brush", "2000.11.12 17:15"));
+			taskRepository.save(new Task(null, "clean the hall", "use brush", "2000.11.13 17:15"));
+			taskRepository.save(new Task(null, "clean the staircase", "use brush", "2000.11.14 17:15"));
+		};
+	}
 }
