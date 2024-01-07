798c21a654717ee72ef6a42a4db866e3815844e2
FilipPietryga <filippatrykpietryga@gmail.com>
2024-01-07 18:51:32 +0100

    finished the implemetation of the impl service class

--- backend/todoapp/src/main/java/my/own/todoapp/service/impl/TaskServiceImpl.java
+++ backend/todoapp/src/main/java/my/own/todoapp/service/impl/TaskServiceImpl.java
@@ -1,38 +1,60 @@
 package my.own.todoapp.service.impl;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import my.own.todoapp.model.Task;
+import my.own.todoapp.repo.TaskRepository;
 import my.own.todoapp.service.TaskService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.Collection;
 
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class TaskServiceImpl implements TaskService {
+    private final TaskRepository taskRepository;
     @Override
     public Task post(Task task) {
-        return null;
+        log.info("Task of {} have been added to the repository", task);
+        taskRepository.save(task);
+        return task;
     }
 
     @Override
     public Task update(Task task) {
-        return null;
+        log.info("Task of {} have been updated on the repository", task);
+        taskRepository.deleteById(task.getId());
+        taskRepository.save(task);
+        return task;
     }
 
     @Override
     public Boolean delete(Long id) {
-        return null;
+        log.info("Task of id={} have been removed from the repository", id);
+        taskRepository.deleteById(id);
+        return true;
     }
 
     @Override
     public Task get(Long id) {
-        return null;
+        log.info("Task of id={} have been loaded from the repository", id);
+        return taskRepository.getById(id);
     }
 
     @Override
     public Collection<Task> getAll() {
-        return null;
+        log.info("Tasks have been loaded from the repository");
+        return taskRepository.findAll();
     }
 
     @Override
     public Collection<Task> getN(int limit) {
-        return null;
+        log.info("Tasks with limit={} have been loaded from the repository", limit);
+        return (Collection<Task>) taskRepository.findAll(PageRequest.of(0, limit));
     }
 }
